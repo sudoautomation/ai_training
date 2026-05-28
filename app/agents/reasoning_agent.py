92868310d73945d4ee10361d04817ff9f4581dc0
Teja9755 <tejavardhanpoluru@gmail.com>
2026-05-28 05:38:02 +0000

    add dotenv

--- app/agents/reasoning_agent.py
+++ app/agents/reasoning_agent.py
@@ -2,6 +2,9 @@ from langchain_google_genai import (
     ChatGoogleGenerativeAI
 )
 
+from dotenv import load_dotenv
+
+load_dotenv()
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0.1
